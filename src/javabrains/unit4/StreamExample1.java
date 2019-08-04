ce2f7d528c8b91374d17082c49f2878a43f10b79
LukaszP <lukaszp4@onet.eu>
2019-08-04 20:06:19 +0200

    6th commit

--- src/javabrains/unit4/StreamExample1.java
+++ src/javabrains/unit4/StreamExample1.java
@@ -4,6 +4,7 @@ import javabrains.unit1.Person;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamExample1 {
 
@@ -24,6 +25,28 @@ public class StreamExample1 {
                 // of this stream that match the given predicate
                 .forEach(p -> System.out.println(p.getFirstName()));
             // we want to isolate the people, who have the first name, beginning with
+
+        // Stream<Person> stream = people.stream(); however it is easier to do
+            long count  = people.stream()  // which can assigns a another local variable, however we also have to case it
+                    .filter(p -> p.getLastName().startsWith("C"))
+                    .count(); // it counts the number of element of the time of termination
+
+            long count1 = people.stream()
+                    .filter(p -> p.getLastName().startsWith("D"))
+                    .count();
+
+        System.out.println("The number of records beginning with C" + count);
+        System.out.println("The number of records beginning with D" + count1);
     }
+
+    // a stream is a new view of a collection
+    // the source of the stream provides the elements on the conveyor belt
+    // at the end we have a terminal operation
+
+    // WHY LAMBDAS ?
+    // Enables functional programming
+    // Readable and concise code
+    // Easier-to-use APIs and libraries
+    // Enables support for parallel processing
 
 }
