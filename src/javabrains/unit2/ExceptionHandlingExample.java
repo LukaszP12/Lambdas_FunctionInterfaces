d43ffb8c4aa9c283f67f1a38743ab1ff7b7e6db5
LukaszP <lukaszp4@onet.eu>
2019-08-04 17:12:43 +0200

    3rd commit

---
+++ src/javabrains/unit2/ExceptionHandlingExample.java
@@ -0,0 +1,28 @@
+package javabrains.unit2;
+
+import java.util.function.BiConsumer;
+
+public class ExceptionHandlingExample {
+
+    public static void main(String[] args) {
+        int [] someNumbers = {1,2,3,4};
+        int key = 0;
+
+        process(someNumbers, key, (v,k) -> {
+            try {
+                System.out.println(v / k);
+            } catch (ArithmeticException e){
+                System.out.println("You cannot divide by zero");
+            }
+        });
+
+    }
+
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer,Integer> consumer){
+        for (int i : someNumbers){
+                consumer.accept(i, key);
+        }
+
+    }
+
+}
